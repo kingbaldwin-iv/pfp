465c3761907a748ec0cac107cddbab8cfbc53ac6
0xb <113536612+kingbaldwin-iv@users.noreply.github.com>
2025-10-22 22:12:10 +0200

    Update pfp.py

--- pfp.py
+++ pfp.py
@@ -12,27 +12,20 @@ class Plotter:
         self.n = n
         self.is_bw = is_bw
         self.bounds = bounds
-    def process(self, pos = None, is_bw = False):
+    def process(self, pos, is_bw = False):
         x = np.zeros(self.n)
         y = np.zeros(self.n)
         r = np.zeros(self.n)
         g = np.zeros(self.n)
         b = np.zeros(self.n)
-        if pos is None:
-            x[0] = random.randint(0,self.bounds)
-            y[0] = random.randint(0,self.bounds)
-        else:
-            x[0] = pos[0]
-            y[0] = pos[1]
+        x[0] = pos[0]
+        y[0] = pos[1]
         for i in range(1, self.n):
             v = list(bin(random.randint(1, 32))[2:])
             v = [0] * (5-len(v)) + v
             it = zip(v, [x,y,r,g,b])
-            for com, val in (it):
-                if com == '1':
-                    val[i] = val[i-1] + 1
-                else:
-                    val[i] = val[i-1] - 1
+            for com, val in it:
+                val[i] = val[i-1] + 1 if com == '1' else val[i-1] - 1
         x = x % self.bounds
         y = y % self.bounds
         r = r % 256
@@ -49,16 +42,11 @@ class Plotter:
         poses = np.linspace(0,self.bounds//2,self.rounds)
         pss = set()
         for i in poses:
-            pss.add((i,i))
-            pss.add((i,self.bounds-i))
-            pss.add((self.bounds-i,i))
-            pss.add((self.bounds-i,self.bounds-i))
+            pos = pos.union({(i,i),(i,self.bounds-i),(self.bounds-i,i),(self.bounds-i,self.bounds-i)})
         return pss
     def plot(self):
         poses = self.get_pos()
-        plotable = list()
-        for i in poses:
-            plotable.append(self.process(i))
+        plotable = [self.process(i) for i in poses]
         plt.title(f"5D Random Walk ({self.n} steps)")
         for x,y,c in plotable:
             plt.scatter(x,y,color=c,s=1)
@@ -67,9 +55,6 @@ class Plotter:
         ax.set_ylim([0, self.bounds])
         plt.savefig("qq.pdf",format="pdf")
         plt.show()
-
-
-
 
 #plotter = Plotter(rounds=8,n=100000,bounds=1000)
 #plotter.plot()
